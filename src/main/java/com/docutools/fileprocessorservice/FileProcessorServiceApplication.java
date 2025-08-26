a62972f3a95ab62f9d0fb70dd1f3c13f38070d92
Chitrak07 <deepak119907@gmail.com>
2025-08-26 21:13:46 +0530

    fix

--- src/main/java/com/docutools/fileprocessorservice/FileProcessorServiceApplication.java
+++ src/main/java/com/docutools/fileprocessorservice/FileProcessorServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+// Add this annotation to explicitly define the packages to scan
+@ComponentScan(basePackages = "com.docutools.fileprocessorservice")
 @SpringBootApplication
 public class FileProcessorServiceApplication extends SpringBootServletInitializer {
 
